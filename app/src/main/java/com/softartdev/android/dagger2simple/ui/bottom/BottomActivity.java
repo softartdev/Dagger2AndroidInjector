183f606357b86ad603be009c621e98039532e19d
ArturB <artik222012@gmail.com>
2017-05-26 13:23:52 +0500

    adding fragments

--- app/src/main/java/com/softartdev/android/dagger2simple/ui/bottom/BottomActivity.java
+++ app/src/main/java/com/softartdev/android/dagger2simple/ui/bottom/BottomActivity.java
@@ -5,43 +5,48 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.softartdev.android.dagger2simple.R;
+import com.softartdev.android.dagger2simple.ui.bottom.fragments.dashboard.DashboardFragment;
+import com.softartdev.android.dagger2simple.ui.bottom.fragments.home.HomeFragment;
+import com.softartdev.android.dagger2simple.ui.bottom.fragments.notifications.NotificationsFragment;
 
-public class BottomActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+public class BottomActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    DashboardFragment mDashboardFragment;
+    HomeFragment mHomeFragment;
+    NotificationsFragment mNotificationsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                if (mHomeFragment == null) {
+                    mHomeFragment = new HomeFragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.content, mHomeFragment).commit();
+                return true;
+            case R.id.navigation_dashboard:
+                if (mDashboardFragment == null) {
+                    mDashboardFragment = new DashboardFragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.content, mDashboardFragment).commit();
+                return true;
+            case R.id.navigation_notifications:
+                if (mNotificationsFragment == null) {
+                    mNotificationsFragment = new NotificationsFragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.content, mNotificationsFragment).commit();
+                return true;
+        }
+        return false;
+    }
 }
