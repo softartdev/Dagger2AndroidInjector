5a000e57e1195fa272288f0a90965bee436b5dbb
ArturB <artik222012@gmail.com>
2017-05-30 17:27:53 +0500

    create ScrollingActivity

--- app/src/main/java/com/softartdev/android/dagger2simple/ui/main/MainActivity.java
+++ app/src/main/java/com/softartdev/android/dagger2simple/ui/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.softartdev.android.dagger2simple.ui.main;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 import com.softartdev.android.dagger2simple.R;
 import com.softartdev.android.dagger2simple.ui.bottom.BottomActivity;
+import com.softartdev.android.dagger2simple.ui.scroll.ScrollingActivity;
 import com.softartdev.android.dagger2simple.ui.second.SecondActivity;
 
 import javax.inject.Inject;
@@ -19,6 +21,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Inject NetworkApi networkApi;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AndroidInjection.inject(this);
@@ -32,6 +35,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         Button secondButton = (Button) findViewById(R.id.main_button);
         secondButton.setOnClickListener(this);
 
+        Button scrollButton = (Button) findViewById(R.id.scroll_button);
+        scrollButton.setOnClickListener(this);
+
         Button bottomButton = (Button) findViewById(R.id.bottom_button);
         bottomButton.setOnClickListener(this);
     }
@@ -41,6 +47,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.main_button:
                 startActivity(new Intent(this, SecondActivity.class));
+                break;
+            case R.id.scroll_button:
+                startActivity(new Intent(this, ScrollingActivity.class));
                 break;
             case R.id.bottom_button:
                 startActivity(new Intent(this, BottomActivity.class));
